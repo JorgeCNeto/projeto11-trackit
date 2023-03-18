7569d230ac5680e7f327807290689486f3ac10ba
Jorge Neto <jorge.c.neto@hotmail.com>
2023-03-18 17:35:57 -0300

    cadastro ok

--- src/pages/Login/Cadastro.js
+++ src/pages/Login/Cadastro.js
@@ -4,9 +4,13 @@ import { Link } from "react-router-dom"
 import { useEffect, useState } from "react"
 import axios from "axios"
 import {URL_CADASTRO} from "../../constants/urls"
+import { StyleSheetManager } from "styled-components"
 
 export default function Login(){
-    const[cadastro, setCadastro] = useState(undefined)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [image, setImage] = useState("")
 
     useEffect(() => {
        
@@ -14,19 +18,32 @@ export default function Login(){
     }, [])
 
     function cadastramento(){
-        axios.post(`${URL_CADASTRO}`)
-        .then(res => console.log(res))
-        .catch(err => console.log(err.respode.data))
+        const cadastro = {email, name, image, password}
+        console.log(cadastro)
+        const promise = axios.post(URL_CADASTRO, cadastro)
+        promise.then(res => alert("foi a requisição"))
+        promise.catch(err => console.log(err.response.data))
+
+        // axios.post(`${URL_CADASTRO}`)
+        // .then(res => {
+        //     email: email,
+        //     name: name,
+        //     image: image,
+        //     password: password
+        // })
+        // .catch(err => console.log(err.respode.data))
     }
    
     return (
         <Corpo>
             <Logo src={logo} />
-            <InputLogin placeholder="email" />
-            <InputLogin placeholder="senha" />
-            <InputLogin placeholder="nome" />
-            <InputLogin placeholder="foto" />
-            <Botao onClick={cadastramento}>Entrar</Botao>
+            <form onSubmit={cadastramento}>
+                <InputLogin type="email" placeholder="email" required value={email} onChange={e => setEmail(e.target.value)}/>
+                <InputLogin type="password" placeholder="senha" required value={password} onChange={e => setPassword(e.target.value)}/>
+                <InputLogin type="text" placeholder="nome" required value={name} onChange={e => setName(e.target.value)}/>
+                <InputLogin type="text" placeholder="foto" required value={image} onChange={e => setImage(e.target.value)}/>
+                <Botao type="submit">Entrar</Botao>
+            </form>
             <ENDERECO>
                 <Link to="/">
                     Já tem uma conta? Faça login!
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,15 +1,30 @@
 import {Corpo, Logo, InputLogin, Botao, ENDERECO} from "./LoginStyle"
 import logo from "../../assets/logo.svg"
 import { Link } from "react-router-dom"
+import {URL_LOGIN} from "../../constants/urls"
+import axios from "axios"
+import { useState } from "react"
 
 export default function Login(){
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function login(){
+        const logar = {email, password}
+        console.log(logar)
+        const promise = axios.post(URL_LOGIN, logar)
+        promise.then(res => alert("foi a requisição"))  //ir para o link: /hoje
+        promise.catch(err => console.log(err.responde.data)/*alert("Usuário e/ou senha inválidos!")*/)
+    }
 
     return (
         <Corpo>
             <Logo src={logo} />
-            <InputLogin placeholder="email" />
-            <InputLogin placeholder="senha" />
-            <Botao>Entrar</Botao>
+            {/* <form onSubmit={login}> */}
+                <InputLogin placeholder="email" required value={email} onChange={e => setEmail(e.target.value)}/>
+                <InputLogin placeholder="senha" required value={password} onChange={e => setPassword(e.target.value)}/>
+                <Botao onClick={login} type="submit">Entrar</Botao>
+            {/* </form> */}
             <ENDERECO>
                 <Link to="/cadastro">
                     Não tem uma conta? Cadastre-se!
